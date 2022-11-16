2025f4a7643a468825c41e51f11fe0ec32e0e2f8
Léo Arsenin <t-leoarsenin@microsoft.com>
2022-11-16 11:02:45 +0100

    delete template code secret name

--- main.py
+++ main.py
@@ -9,11 +9,9 @@ from azure.identity import DefaultAzureCredential
 
 keyVaultName = "encodingkv"
 KVUri = f"https://{keyVaultName}.vault.azure.net"
-secretName = "mySecret"
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
-retrieved_secret = client.get_secret(secretName)
 
 #Aure variables
 
